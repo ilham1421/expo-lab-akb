ffec6e57f507e6afaf6ed55033bd474705c76a27
ilham1421 <105841105822@student.unismuh.ac.id>
2025-07-09 09:32:04 +0800

    tugas2

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {
+  Animated,
   Dimensions,
   Image,
   StyleSheet,
@@ -13,48 +14,48 @@ const { width } = Dimensions.get("window");
 const imageData = [
   {
     id: 1,
-    main: "https://img1.picmix.com/output/stamp/normal/6/9/9/4/1614996_ce8a5.gif",
-    alt: "https://i.pinimg.com/originals/6d/d9/d1/6dd9d1ce4ed795ccd8db603ba764eac0.gif",
+    main: "https://picsum.photos/200/200?random=1",
+    alt: "https://picsum.photos/200/200?random=11",
   },
   {
     id: 2,
-    main: "https://i.pinimg.com/originals/e8/84/99/e884990a7863c90811e37b275cae3f0c.gif",
-    alt: "https://i.pinimg.com/originals/b0/85/dc/b085dc97e96eb1b097c7733bbdb04e15.gif",
+    main: "https://picsum.photos/200/200?random=2",
+    alt: "https://picsum.photos/200/200?random=12",
   },
   {
     id: 3,
-    main: "https://media.tenor.com/aHIng-Wks00AAAAM/tanya-degurechaff.gif",
-    alt: "https://media.tenor.com/qL1c_sfmbkEAAAAM/tanya-youjo-senki.gif",
+    main: "https://picsum.photos/200/200?random=3",
+    alt: "https://picsum.photos/200/200?random=13",
   },
   {
     id: 4,
-    main: "https://media.tenor.com/pl_gjRkbSLQAAAAM/gintama-gintoki.gif",
-    alt: "https://i.pinimg.com/originals/50/b9/a1/50b9a1a49ddb7a40acbb35066e352cb3.gif",
+    main: "https://picsum.photos/200/200?random=4",
+    alt: "https://picsum.photos/200/200?random=14",
   },
   {
     id: 5,
-    main: "https://i.pinimg.com/originals/af/1d/61/af1d61b035f1ddf08c737f646332caae.gif",
-    alt: "https://i.redd.it/fzf81y4wu3zb1.gif",
+    main: "https://picsum.photos/200/200?random=5",
+    alt: "https://picsum.photos/200/200?random=15",
   },
   {
     id: 6,
-    main: "https://i.pinimg.com/originals/8d/ef/0b/8def0b35b5c443dd3db6de2cfb3dda18.gif",
-    alt: "https://i.pinimg.com/originals/fc/ba/c4/fcbac4d9b14cdd6a4bbc5f9491c02962.gif",
+    main: "https://picsum.photos/200/200?random=6",
+    alt: "https://picsum.photos/200/200?random=16",
   },
   {
     id: 7,
-    main: "https://media.tenor.com/jwucCvtz6I4AAAAM/records-of-ragnarok-buddha.gif",
-    alt: "https://media.tenor.com/lXzGE7g6dNQAAAAM/shuumatsu-no-valkyrie-ragnarok.gif",
+    main: "https://picsum.photos/200/200?random=7",
+    alt: "https://picsum.photos/200/200?random=17",
   },
   {
     id: 8,
-    main: "https://media.tenor.com/yritcPNRk3gAAAAM/overlord-edit.gif",
-    alt: "https://media.tenor.com/XDWz8vUSvoYAAAAM/overlord-renner.gif",
+    main: "https://picsum.photos/200/200?random=8",
+    alt: "https://picsum.photos/200/200?random=18",
   },
   {
     id: 9,
-    main: "https://static.wikia.nocookie.net/e5f4ac13-fa9e-4fb0-938e-d668a39dca93",
-    alt: "https://giffiles.alphacoders.com/174/174444.gif",
+    main: "https://picsum.photos/200/200?random=9",
+    alt: "https://picsum.photos/200/200?random=19",
   },
 ];
 
@@ -94,13 +95,15 @@ export default function Index() {
           onPress={() => handleImagePress(item.id)}
           activeOpacity={0.8}
         >
-          <View style={[styles.imageContainer, { transform: [{ scale }] }]}>
+          <Animated.View
+            style={[styles.imageContainer, { transform: [{ scale }] }]}
+          >
             <Image
               source={{ uri: imageUri }}
               style={styles.image}
               resizeMode="cover"
             />
-          </View>
+          </Animated.View>
         </TouchableOpacity>
       );
     });
@@ -142,14 +145,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "center",
-    width: width - 40,
-    maxWidth: 360,
+    alignItems: "center",
+    width: 330,
+    height: 330,
   },
   gridItem: {
-    width: (width - 80) / 3,
-    height: (width - 80) / 3,
-    maxWidth: 110,
-    maxHeight: 110,
+    width: 100,
+    height: 100,
     margin: 5,
     overflow: "visible",
   },
